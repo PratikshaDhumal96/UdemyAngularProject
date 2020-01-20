9d508dba0eddc4a69f46420b7ab92cbd1841ea3e
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2020-01-20 19:31:51 +0530

    "CourseProject(The basic (UI completed))"

---
+++ UdemyCRUD/src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+
+export  class Ingredient {
+    // typescript offers a shortcut:- how to automatically recieve the arguments that we want to assign in the constructor that instantly assign them .
+constructor ( public name : string , public amount :number){ }
+
+
+
+    // public name : string;
+    // public amount : number;
+
+    // constructor ( name : string ,amount : number){
+    //     this.name = name;
+    //     this.amount = amount;
+
+    // }
+}
