9e5e8be198dab1c13c50f5e82b9acc29a18d3d4e
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2020-01-21 16:00:27 +0530

    create a custome component. also create nested components. what is exactly string Interpollation..is in that..

---
+++ BasicDirective/src/app/server/server.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  serverId : number = 10
+  status : string = 'offline'
+
+  getServerStatus(){
+    return this.status;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
