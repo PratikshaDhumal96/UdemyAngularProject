ccaec81dc9765f674994861e0a11aa24c5073110
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2020-01-30 11:56:23 +0530

    ngFor and ngIf used for display even odd numbers

--- Directives/src/app/practice-directives/practice-directives.component.ts
+++ Directives/src/app/practice-directives/practice-directives.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticeDirectivesComponent implements OnInit {
 
+  // numbers = [1,2,3,4,5]
+  oddNumbers=[1,3,5]
+  evenNumbers= [2,4]
+
+  onlyOdd = false
+
   constructor() { }
 
   ngOnInit() {
