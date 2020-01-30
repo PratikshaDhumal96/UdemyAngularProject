cb21f151e55e4b05aae7147a493904a99813107d
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2020-01-30 14:31:42 +0530

    using the Rendere to build a better Attribute Directive.

--- Directives/src/app/app.module.ts
+++ Directives/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PracticeDirectivesComponent } from './practice-directives/practice-directives.component';
-import { BasicHighlightDirectiveDirective } from './basic-highlight-directive.directive';
+import { BasicHighlightDirectiveDirective } from './basic-highlight/basic-highlight-directive.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     PracticeDirectivesComponent,
-    BasicHighlightDirectiveDirective
+    BasicHighlightDirectiveDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
